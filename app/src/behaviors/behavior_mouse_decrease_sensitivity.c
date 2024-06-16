53b2925855b72fcea20ee3d38c552d6f026cc0d0
RyanBlaney <ryanblaney2004@gmail.com>
2024-06-16 13:48:45 -0700

    Added sensitivity for the mouse

---
+++ app/src/behaviors/behavior_mouse_decrease_sensitivity.c
@@ -0,0 +1,34 @@
+
+#define DT_DRV_COMPAT zmk_behavior_mouse_decrease_sensitivity
+
+#include <zephyr/device.h>
+#include <drivers/behavior.h>
+#include <zephyr/logging/log.h>
+#include <zmk/behavior.h>
+#include <zmk/mouse/hid.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
+
+static int behavior_mouse_decrease_sensitivity_init(const struct device *dev) { return 0; }
+
+static int on_decrease_sensitivity_pressed(struct zmk_behavior_binding *binding,
+                                           struct zmk_behavior_binding_event event) {
+    zmk_mouse_decrease_sensitivity();
+    return 0;
+}
+
+static const struct behavior_driver_api behavior_mouse_decrease_sensitivity_driver_api = {
+    .binding_pressed = on_decrease_sensitivity_pressed,
+    .binding_released = NULL,
+};
+
+#define MKP_INST(n)                                                                                \
+    BEHAVIOR_DT_INST_DEFINE(n, behavior_mouse_decrease_sensitivity_init, NULL, NULL, NULL,         \
+                            POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                      \
+                            &behavior_mouse_decrease_sensitivity_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(MKP_INST)
+
+#endif

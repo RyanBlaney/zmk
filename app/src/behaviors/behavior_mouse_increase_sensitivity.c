14cf63753c30aab9079e38d7ad367a6f063afa49
RyanBlaney <ryanblaney2004@gmail.com>
2024-06-16 15:22:27 -0700

    Added sensitivity for the mouse

--- app/src/behaviors/behavior_mouse_increase_sensitivity.c
+++ app/src/behaviors/behavior_mouse_increase_sensitivity.c
@@ -1,4 +1,5 @@
 
+
 #define DT_DRV_COMPAT zmk_behavior_mouse_increase_sensitivity
 
 #include <zephyr/device.h>
@@ -9,19 +10,22 @@
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
-
 static int behavior_mouse_increase_sensitivity_init(const struct device *dev) { return 0; }
 
-static int on_increase_sensitivity_pressed(struct zmk_behavior_binding *binding,
-                                           struct zmk_behavior_binding_event event) {
+static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
+                                     struct zmk_behavior_binding_event event) {
     zmk_mouse_increase_sensitivity();
     return 0;
 }
 
+static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
+                                      struct zmk_behavior_binding_event event) {
+    return 0;
+}
+
 static const struct behavior_driver_api behavior_mouse_increase_sensitivity_driver_api = {
-    .binding_pressed = on_increase_sensitivity_pressed,
-    .binding_released = NULL,
+    .binding_pressed = on_keymap_binding_pressed,
+    .binding_released = on_keymap_binding_released,
 };
 
 #define MKP_INST(n)                                                                                \
@@ -30,5 +34,3 @@ static const struct behavior_driver_api behavior_mouse_increase_sensitivity_driv
                             &behavior_mouse_increase_sensitivity_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(MKP_INST)
-
-#endif
